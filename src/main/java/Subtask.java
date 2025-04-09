bcc6bb47ec0ae24108f4d7b3352fe0568ebcfbdf
shanekat@student <shanekat@student.21-school.ru>
2025-04-09 15:47:01 +0300

    FileBackedTasksManager done

--- src/main/java/Subtask.java
+++ src/main/java/Subtask.java
@@ -5,11 +5,13 @@ public class Subtask extends Task {
     public Subtask(String name, String description, int idEpic) {
         super(name, description);
         this.idEpic = idEpic;
+        this.type = TypeTask.SUBTASK;
     }
 
     public int getEpicId() {
         return idEpic;
     }
+
 
     @Override
     public String toString() {
